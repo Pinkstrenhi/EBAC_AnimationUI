e366ebfab5561fb4ceb0dca3db7b07da8511662e
Pinkstrenhi <82385580+Pinkstrenhi@users.noreply.github.com>
2023-05-21 20:47:17 -0300

    Menu and Hotkey

--- Assets/Scripts/Extensions/ExtensionsUtils.cs
+++ Assets/Scripts/Extensions/ExtensionsUtils.cs
@@ -4,41 +4,62 @@ using UnityEngine;
 
 public static class ExtensionsUtils 
 {
-    public static void Scale(this Transform scaleTransform, float scaleSize = 1.2f)
-    {
-        scaleTransform.localScale = Vector3.one * scaleSize; 
-    }
-    public static void ScaleVector(this Vector3 scaleVector, float scaleSize = 1.2f)
-    {
-        //scaleVector.localScale = Vector3.one * scaleSize; 
-    }
-    public static void ScaleGameObject(this GameObject scaleGameObject, float scaleSize = 1.2f)
-    {
-        scaleGameObject.transform.localScale = Vector3.one * scaleSize; 
-    }
-
-    public static T GetRandom<T>(this List<T> list)
-    {
-        return list[Random.Range(0, list.Count)];
-    }
-
-    public static T GetRandom<T>(this T[] array)
-    {
-        if (array.Length == 0)
+    #if UNITY_EDITOR
+        [UnityEditor.MenuItem("Ebac/Test01")]
+        public static void Test01()
         {
-            return default(T);
+            Debug.Log("Test01");
+        }
+        [UnityEditor.MenuItem("Ebac/Test02 %g")]
+        public static void Test02()
+        {
+            Debug.Log("Test02");
+        }
+    #endif
+    
+    #region Scale
+
+        public static void Scale(this Transform scaleTransform, float scaleSize = 1.2f)
+        {
+            scaleTransform.localScale = Vector3.one * scaleSize; 
+        }
+        public static void ScaleVector(this Vector3 scaleVector, float scaleSize = 1.2f)
+        {
+            //scaleVector.localScale = Vector3.one * scaleSize; 
+        }
+        public static void ScaleGameObject(this GameObject scaleGameObject, float scaleSize = 1.2f)
+        {
+            scaleGameObject.transform.localScale = Vector3.one * scaleSize; 
         }
 
-        return array[Random.Range(0, array.Length)];
-    }
+    #endregion
+    
+    #region Random
 
-    public static T GetRandomButNotTheSame<T>(this List<T> list, T unique)
-    {
-        if (list.Count == 1)
+        public static T GetRandom<T>(this List<T> list)
         {
-            return unique;
+            return list[Random.Range(0, list.Count)];
         }
-        var randomIndex = Random.Range(0, list.Count);
-        return list[randomIndex];
-    }
+
+        public static T GetRandom<T>(this T[] array)
+        {
+            if (array.Length == 0)
+            {
+                return default(T);
+            }
+
+            return array[Random.Range(0, array.Length)];
+        }
+
+        public static T GetRandomButNotTheSame<T>(this List<T> list, T unique)
+        {
+            if (list.Count == 1)
+            {
+                return unique;
+            }
+            var randomIndex = Random.Range(0, list.Count);
+            return list[randomIndex];
+        }
+
+    #endregion
 }
